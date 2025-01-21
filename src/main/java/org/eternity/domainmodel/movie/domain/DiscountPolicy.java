42788fa4ff9bf54aae7343678177d59a496af2b7
조영호 <aeternum9342@gmail.com>
2025-01-21 21:36:15 +0900

    프로젝트 초기화

---
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -0,0 +1,29 @@
+package org.eternity.domainmodel.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="policy_type")
+@NoArgsConstructor @Getter
+public abstract class DiscountPolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name="POLICY_ID")
+    private Set<DiscountCondition> conditions = new HashSet<>();
+
+    public DiscountPolicy(Set<DiscountCondition> conditions) {
+        this.conditions = conditions;
+    }
+
+    public Set<DiscountCondition> getConditions() {
+        return conditions;
+    }
+}
---
+++ src/test/java/org/eternity/movie/persistence/JpaLazyTest.java
@@ -0,0 +1,44 @@
+package org.eternity.movie.persistence;
+
+import jakarta.persistence.EntityManager;
+import org.eternity.domainmodel.generic.Money;
+import org.eternity.domainmodel.movie.domain.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Set;
+
+@DataJpaTest(showSql = false)
+public class JpaLazyTest {
+	@Autowired
+	private EntityManager em;
+
+	@Test
+	public void add_discount_condition() {
+		DiscountPolicy policy1 =
+				new AmountDiscountPolicy(
+						Money.wons(1000),
+						Set.of(new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(11, 0)),
+								new SequenceCondition(1)));
+		DiscountPolicy policy2 =
+				new PercentDiscountPolicy(
+						0.1,
+						Set.of(new SequenceCondition(1),
+								new SequenceCondition(3)));
+
+		em.persist(policy1);
+		em.persist(policy2);
+		em.flush();
+		em.clear();
+
+		List<DiscountPolicy> policies = em.createQuery("select p from DiscountPolicy p").getResultList();
+
+		for(DiscountPolicy policy : policies) {
+			policy.getConditions().size();
+		}
+	}
+}
